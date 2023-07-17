19b28947e62874324f7f130059278ec268484165
Collins Murgor <vintoncollins702@gmail.com>
2023-07-17 17:06:48 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Gallery from "./Gallery.js";
+import { Profile } from "./Profile.js";
+
+export default function App() {
+  return (
+    <>
+      <Profile />
+      <Gallery />
+    </>
+  );
+}
---
+++ src/Gallery.js
@@ -0,0 +1,12 @@
+import { Profile } from "./Profile";
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientist</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
